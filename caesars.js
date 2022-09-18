e6abc2ec488995f80e0530d3c30d70e9dd7bd17a
jagu-ms <mohamedseddiki20@gmail.com>
2022-09-18 06:19:50 +0100

    Phone Validator and Caesar Convertor

--- caesars.js
+++ caesars.js
@@ -1,1 +1,22 @@
+function rot13(str) {
+  // Regax matches alphabetical characters
+  let regax = /[a-zA-Z]/;
 
+  return str.split("").map(char => {
+    const charCode = char.charCodeAt();
+    const transformed = charCode + 13;
+
+    // check if the char is an alphabet
+    if(regax.test(char)) {
+      if(transformed <= 90) {
+        return String.fromCharCode(transformed)
+      } else {
+        return String.fromCharCode(transformed - 26)
+      }
+    } else {
+      return char;
+    }
+  }).join("");
+}
+
+rot13("SERR PBQR PNZC");
